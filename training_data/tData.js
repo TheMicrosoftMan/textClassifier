e9a248d4863d89a67f97f2c2ecfda1bf8c21e367
YZ <yurazelenko@windowslive.com>
2018-11-10 19:00:35 +0200

    added official_business and publicistik styles; translated styles name to ukraine language

--- training_data/tData.js
+++ training_data/tData.js
@@ -160,4 +160,217 @@ module.exports = [{
 }, {
     input: "\"Good day to you, niece,\" said Baba Yaga.",
     output: "rozmovny"
+}, {
+    input: "Thank you so much for taking the time to meet with me to discuss selling my handmade sweaters in your wonderful shop.",
+    output: "official_business"
+}, {
+    input: "As I mentioned in our conversation, I\'ve been a customer of your store since I used my third-grade allowance to buy my very first pair of knitting needles.",
+    output: "official_business"
+}, {
+    input: "I\'m honored that you\'d consider selling one of my original creations at The Yarn Company alongside your own work.",
+    output: "official_business"
+}, {
+    input: "We discussed a trial consignment arrangement in which a portion of the sales would go to the store.",
+    output: "official_business"
+}, {
+    input: "I\'m available most afternoons at 555-555-5555, or you can email me at email@email.com, and I\'ll respond to your message ASAP.",
+    output: "official_business"
+}, {
+    input: "I\'m writing today to invite you or another representative from your company to speak at the annual Metropolitan Business Conference, which will be held at North Branch Hotel, January 15 - 18, 2019.",
+    output: "official_business"
+}, {
+    input: "The theme of our upcoming conference is finding and hiring employees who fit company culture.",
+    output: "official_business"
+}, {
+    input: "With the growth that your company has seen in the past five years, I believe you would have much to offer our audience.",
+    output: "official_business"
+}, {
+    input: "As part of the speaker\'s package, we offer a modest honorarium and a table for six at the Saturday night dinner.",
+    output: "official_business"
+}, {
+    input: "If you have any questions or wish to know more about the speaking opportunity, please let me know.",
+    output: "official_business"
+}, {
+    input: "My cell phone number is 555-555-5555, and my email is joan.lau@myemail.com.",
+    output: "official_business"
+}, {
+    input: "I look forward to hearing from you.",
+    output: "official_business"
+}, {
+    input: "Thank you for your consideration.",
+    output: "official_business"
+}, {
+    input: "Thank you for your speedy and beautiful work fixing our floors after our plumbing mishap this spring.",
+    output: "official_business"
+}, {
+    input: "Although I would have preferred to meet you and your crew under different circumstances – a planned renovation, say, and not during our busiest season so grateful that my neighbor recommended you.",
+    output: "official_business"
+}, {
+    input: "Our floors have never looked better.",
+    output: "official_business"
+}, {
+    input: "Customers comment on them at least once a week, which is unusual to say the least! I always make sure to give them your information and pass along my recommendation.",
+    output: "official_business"
+}, {
+    input: "Thank you, again, for fitting us in at the last minute and getting us back up and running so quickly.",
+    output: "official_business"
+}, {
+    input: "Best regards.",
+    output: "official_business"
+}, {
+    input: "Thank you for taking the time to talk with me today. ",
+    output: "official_business"
+}, {
+    input: "I sincerely appreciate the time you spent reviewing my career goals and recommending strategies for achieving them. ",
+    output: "official_business"
+}, {
+    input: "Your advice was very helpful and gave me a new perspective on available opportunities.",
+    output: "official_business"
+}, {
+    input: "I especially appreciate your offer to connect me to others in your network. ",
+    output: "official_business"
+}, {
+    input: "I plan on following up with the contacts you emailed me right away. ",
+    output: "official_business"
+}, {
+    input: "I will also use the online networking resources you recommended to further my job search.",
+    output: "official_business"
+}, {
+    input: "Any additional suggestions you may have would be welcome.",
+    output: "official_business"
+}, {
+    input: "I\'ll update you as my search progresses.",
+    output: "official_business"
+}, {
+    input: "Again, thank you so much for your help.",
+    output: "official_business"
+}, {
+    input: "I greatly appreciate the assistance you have provided me.",
+    output: "official_business"
+}, {
+    input: "Chairman of the Donetsk Regional State Administration Oleksandr Kuts informed this on his Facebook page on Friday, when  President of the NATO Parliamentary Assembly Rasa Jukneviciene and Chairman of the Verkhovna Rada of Ukraine Andriy Parubiy visited Donbas.",
+    output: "publicistik"
+}, {
+    input: "Ukraine defends not only its independence but also defends the democratic world - President of the NATO Parliamentary Assembly Rasa Jukneviciene made such a conclusion after visiting Donbas together with Speaker of the Verkhovna Rada of Ukraine Andriy Parubiy,” the head of the region wrote.",
+    output: "publicistik"
+}, {
+    input: "The NATO PA president visited Mariupol, Shyrokyne and Hnutove, talked to the military and civilians who live on both sides of the so-called line of demarcation and cross checkpoints.",
+    output: "publicistik"
+}, {
+    input: "At Mariupol City Council, she held a general meeting with representatives of the Verkhovna Rada of Ukraine, the military commanders of the Joint Forces Operation (JFO) and the heads of law enforcement agencies.",
+    output: "publicistik"
+}, {
+    input: "They discussed mine safety issues, improvement of the conditions for crossing checkpoints and others.",
+    output: "publicistik"
+}, {
+    input: "The NATO PA president will speak about the situation in Donbas during a high-level meeting in Washington.",
+    output: "publicistik"
+}, {
+    input: "And next week, this issue will be raised at the annual session of the NATO Parliamentary Assembly in Halifax (Canada).",
+    output: "publicistik"
+}, {
+    input: "November 10-11, President of Ukraine Petro Poroshenko will make a working visit to the French Republic.",
+    output: "publicistik"
+}, {
+    input: "the press service of the head of state reported",
+    output: "publicistik"
+}, {
+    input: "At the invitation of French President Emmanuel Macron, Petro Poroshenko will take part in the events within the Paris Peace Forum to mark the 100th anniversary of the end of World War I, as well as in a round table",
+    output: "publicistik"
+}, {
+    input: "During the visit, Poroshenko will hold a number of bilateral meetings with leaders of the countries of the world.",
+    output: "publicistik"
+}, {
+    input: "While the end of World War I brought relief to many, it also set in motion economic and ideological changes, many of which still reverberate today.",
+    output: "publicistik"
+}, {
+    input: "Some argue the Great War accelerated women\'s emancipation but it also resulted in substantial numbers of deaths and injuries.",
+    output: "publicistik"
+}, {
+    input: "On November 11, 1918, at 5.15 am, the armistice was signed in a train carriage — the private lounge of French Marshal Ferdinand Foch, supreme commander of the Allies — that was stationed in the Rethondes clearing of the Compiègne forest, 100 kilometres north of Paris.",
+    output: "publicistik"
+}, {
+    input: "The location was deliberately chosen as it was isolated, especially from the press, to carry out the delicate negotiations.",
+    output: "publicistik"
+}, {
+    input: "A full version of the document can be consulted on the website for the French Ministry of Defence\'s history department.",
+    output: "publicistik"
+}, {
+    input: "The only photo of the carriage, probably taken by a rail worker.",
+    output: "publicistik"
+}, {
+    input: "In addition to Marshal Foch, German State Minister Mathias Erzberger, Count Alfred von Oberndorff, a German diplomat, German Navy Captain Ernst Vanselow, General Detlof von Winterfeldt and British Admiral Rosslyn Wemyss all signed the document.",
+    output: "publicistik"
+}, {
+    input: "Representatives of the Allies and Germany agreed to cease hostilities by 11 am.",
+    output: "publicistik"
+}, {
+    input: "In the final hours of the war, soldiers continued to die, often due to the actions of officers who already knew that the armistice had been signed.",
+    output: "publicistik"
+}, {
+    input: "The death of Augustin Trébuchon is one such tragic case — he was shot in the head at 10.50 am and is thought to be the last French soldier killed in the war.",
+    output: "publicistik"
+}, {
+    input: "The American Henry Gunther is officially considered the last person to have died.",
+    output: "publicistik"
+}, {
+    input: "An estimated 2,500 soldiers died between the moment the armistice was signed and the moment the ceasefire came into force.",
+    output: "publicistik"
+}, {
+    input: "Relative peace did not materialise until the Treaty of Versailles was signed in June 1919.",
+    output: "publicistik"
+}, {
+    input: "\"Years and years were just spent sitting at my computer,\" says Billy Brown.",
+    output: "publicistik"
+}, {
+    input: "\"I would only go outside for doctor\'s appointments or dentist\'s appointments.",
+    output: "publicistik"
+}, {
+    input: "\"I would never go outside to socialise or interact with other people.",
+    output: "publicistik"
+}, {
+    input: "My entire existence was online.\"",
+    output: "publicistik"
+}, {
+    input: "In what was a difficult childhood, Mr Brown was taken into care repeatedly during his mother\'s hospital stays for her mental and physical health problems.",
+    output: "publicistik"
+}, {
+    input: "His attendance during his GCSEs was 13% but it was the stress of starting A-levels at college, coupled with breaking his ankle, that caused him to withdraw from the outside world completely.",
+    output: "publicistik"
+}, {
+    input: "Mr Brown, now 24, dropped out of college and spent the following years at home - gaming, in chat rooms and reading about politics.",
+    output: "publicistik"
+}, {
+    input: "He became almost entirely immersed in an online world of \"echo chambers\" where he felt the pull of extremism and cybercrime.",
+    output: "publicistik"
+}, {
+    input: "Mr Brown, from Ashton, Cornwall, says he became increasingly \"eccentric\" and eventually lost touch with reality.",
+    output: "publicistik"
+}, {
+    input: "\"I can count the number of times I went out in a seven-year period on both of my hands,\" he says.",
+    output: "publicistik"
+}, {
+    input: "\"I wasn\'t taking care of myself, I was only taking care of my mother.",
+    output: "publicistik"
+}, {
+    input: "\"There were times when I was suicidal.",
+    output: "publicistik"
+}, {
+    input: "It really took its toll on me.",
+    output: "publicistik"
+}, {
+    input: "\"I wasn\'t sure why I was alive, why I was here...",
+    output: "publicistik"
+}, {
+    input: "I realised if I didn\'t do something I wouldn\'t be here in a year or two years.\"",
+    output: "publicistik"
+}, {
+    input: "He finally decided to seek help and ended up taking part in the Real Ideas Organisation\'s (RIO) Game Changer programme, which aims to encourage young people to develop skills and overcome any issues they face before getting them into work, education or training.",
+    output: "publicistik"
+}, {
+    input: "Now, just 14 months after stepping outside for the first time in years, Mr Brown has come up with his own way of helping people like him to improve their lives - through tabletop gaming.",
+    output: "publicistik"
+}, {
+    input: "His board game, which he also hopes to develop into an app, aims to help young people build on their social skills and overcome any issues they face.",
+    output: "publicistik"
 }];
--- views/js/script.js
+++ views/js/script.js
@@ -13,9 +13,27 @@ function toTextarea(e) {
     textarea.value = e.innerText;
 }
 
+function translateStyle(style) {
+    switch (style) {
+        case "hudozhiny":
+            return "художній";
+        case "science":
+            return "науковий";
+        case "rozmovny":
+            return "розмовний";
+        case "official_business":
+            return "офіційно-діловий";
+        case "publicistik":
+            return "публіцистичний";
+        default:
+            break;
+    }
+}
+
 socket.on("textStyle", function (data) {
     console.log(data.styleText[0][0]);
     let answEl = document.getElementById("answer");
-    answEl.innerHTML = "Стиль цього текстку - " + data.styleText[0][0] + " з ймовірністю " + data.styleText[0][1] + "%";
+    answEl.innerHTML = "Стиль цього текстку - " + translateStyle(data.styleText[0][0]);
+    // answEl.innerHTML = "Стиль цього текстку - " + data.styleText[0][0] + " з ймовірністю " + data.styleText[0][1] + "%";
     answEl.style.visibility = "visible";
 })
